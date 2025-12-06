7d5318053bc9812542449f45d3a3a48754b80605
Oleksandr Oliinyk <aoleynik@hydromax.com.ua>
2025-12-06 13:06:02 +0100

    feat: init vscode extension

---
+++ src/extension.ts
@@ -0,0 +1,13 @@
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('Extension "multicli-agents" is now active!');
+
+    const disposable = vscode.commands.registerCommand('multicli-agents.open', () => {
+        vscode.window.showInformationMessage('SubAgent Manager Initialized!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
